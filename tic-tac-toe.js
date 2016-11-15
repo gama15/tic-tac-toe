c14b91c99d9a071f9cc4fd5099103e62410d204f
Gamaliel <Gammy1524@gmail.com>
2016-11-15 11:28:42 -0800

    added more

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -21,10 +21,54 @@ function squareOne() {
     lineTwo.setAttribute("stroke", "Red")
     lineTwo.setAttribute("stroke-width", "8")
 
+    var circleThree = document.createElementNS(namespace, "circle")
+    circleThree.setAttribute("cx", "55")
+    circleThree.setAttribute("cy", "55")
+    circleThree.setAttribute("r", "45")
+    circleThree.setAttribute("fill", "white")
+
     var canvas = document.getElementById("game-board")
+    canvas.appendChild(circleThree)
     canvas.appendChild(lineOne)
     canvas.appendChild(lineTwo)
+
+    firstSquare = "o"
+
   } else {
+    var backgroundOne = document.createElementNS(namespace, "line")
+    backgroundOne.setAttribute("x1", "95")
+    backgroundOne.setAttribute("y1", "15")
+    backgroundOne.setAttribute("x2", "15")
+    backgroundOne.setAttribute("y2", "95")
+    backgroundOne.setAttribute("stroke", "white")
+    backgroundOne.setAttribute("stroke-width", "8")
+
+    var backgroundTwo = document.createElementNS(namespace, "line")
+    backgroundTwo.setAttribute("x1", "15")
+    backgroundTwo.setAttribute("y1", "15")
+    backgroundTwo.setAttribute("x2", "95")
+    backgroundTwo.setAttribute("y2", "95")
+    backgroundTwo.setAttribute("stroke", "white")
+    backgroundTwo.setAttribute("stroke-width", "8")
+
+    var circleOne = document.createElementNS(namespace, "circle")
+    circleOne.setAttribute("cx", "55")
+    circleOne.setAttribute("cy", "55")
+    circleOne.setAttribute("r", "45")
+    circleOne.setAttribute("fill", "Red")
+
+    var circleTwo = document.createElementNS(namespace, "circle")
+    circleTwo.setAttribute("cx", "55")
+    circleTwo.setAttribute("cy", "55")
+    circleTwo.setAttribute("r", "35")
+    circleTwo.setAttribute("fill", "white")
+
+    var canvas = document.getElementById("game-board")
+    canvas.appendChild(backgroundOne)
+    canvas.appendChild(backgroundTwo)
+    canvas.appendChild(circleOne)
+    canvas.appendChild(circleTwo)
+    firstSquare = "x"
 
   }
 }
